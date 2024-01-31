61edbc91e60cd6e7a368417ff83715ea842b1c6a
seon97un <tjdwns9703@gmail.com>
2024-01-31 12:15:53 +0900

    todobackend

--- src/App.js
+++ src/App.js
@@ -9,11 +9,29 @@ class App extends React.Component {
   constructor(props){
     super(props)
     /// 3개의 객체를 가진 배열을 생성
-    this.state = {items:[{id:0, title:"React", done:true},
-                        {id:1, title:"Angular", done:false},
-                        {id:2, title:"Vue", done:false}]}
-    /// 데이터를 추가하는 함수
+    this.state = {items:[]}
   }
+    
+    // 화면이 보여질 때마다 호출되는 수명주기 함수
+    componentDidMount(){
+        /// ajax 요청 객체 생성
+        let request = new XMLHttpRequest();
+        // 요청 준비
+        request.open('GET', "http://127.0.0.1/todo?userid=steve");
+        // 요청
+        request.send('');
+        // 응답 처리
+        request.addEventListener('load', () => {
+          // json 데이터 출력
+          console.log(request.responseText)
+          // json 문자열을 데이터로 변환
+          let data = JSON.parse(request.responseText);
+          console.log(data)
+          // 서버에서 받아온 데이터를 state에 대입해서 화면에 출력
+          this.setState({items:data.list})
+        })
+    }  
+
     add = (item) => {
       // state는 수정이 가능하지만 원본을 직접 수정하는 것은 불가
       // setState 메서드를 이용해서만 수정이 가능
@@ -29,6 +47,20 @@ class App extends React.Component {
       // 복사한 데이터를 다시 state에 적용
       this.setState({items:thisItems})
     }
+  
+  //삭제를 위한 메서드
+  delete = (item) => {
+    //state나 props의 데이터는 직접 편집 불가능
+    const thisItems = this.state.items;
+
+    //복사본에서 item 제거
+    //filter 함수는 리턴 타입이 boolean인 함수를 매개변수로 받아서 
+    //리턴 결과가 true인 데이터만 모아서 배열로 리턴하는 함수
+    const newItems = thisItems.filter((e) => e.id !== item.id);
+
+    //원본에 다시 복사
+    this.setState({items:newItems})
+  } 
 
   render(){
     //배열을 순회하면서 출력할 내용을 생성
@@ -39,7 +71,7 @@ class App extends React.Component {
       <Paper style = {{margin : 16}}>
           <List>
             {this.state.items.map((item, idx) => (
-              <ToDo item ={item} id = {idx}/>            
+              <ToDo item ={item} id = {idx} delete={this.delete} />            
               ))}
           </List>
       </Paper>
--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -3,27 +3,35 @@ import React from "react";
 import{ListItem, 
       ListItemText, 
       InputBase, 
-      Checkbox} 
-from "@material-ui/core"
+      Checkbox,
+      ListItemSecondaryAction,
+      IconButton
+} from "@material-ui/core"
+
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined"
 
 // react에서 컴포넌트 클래스를 만들기 위해서 Component로부터 상속을 받기
 class ToDo extends React.Component {
-   //생성자S
+   //생성자
    constructor(props){
       //App.js에서 넘겨준 속성들을 자신의 props에 저장
       //this.props.item이라는 속성에 앞에서 전달한 데이터를 저장
       super(props); // 상위 클래스에서 넘겨준 모든 props를 현재 클래스에 저장
+      
       //state 생성
-      //item이라는 이름으로 props 중에서 item 이라는 값을 state로 저장
-
-      //props는 상위 컴포넌트에서 전달한 데이터라서 읽기는 가능하지만
-      //수정이나 삭제가 안 되므로 수정이나 삭제를 하고자 하는 경우는
-      //state로 변환해야 함
       this.state = {item:props.item}
+
+      // App.js에서 넘겨준 삭제 함수를 현재 클래스의 데이터로 변환
+      this.delete = this.props.delete
    }
+
+   //삭제 버튼을 누를 때 호출될 이벤트 핸들러
+   deleteEventhandler = (e) => {
+      this.delete(this.state.item)
+   }
+
    // 화면에 출력할 내용을 리턴하는 메서드
    render(){
-    //자주 사용하는 데이터를 짧게 사용하기 위해서 다른 변수에 대입
     const item = this.state.item;
      return(
        <ListItem>
@@ -39,6 +47,11 @@ class ToDo extends React.Component {
             fullWidth={true}
             />
         </ListItemText>
+        <ListItemSecondaryAction>
+            <IconButton aria-label = "Delete ToDo" onClick={this.deleteEventhandler}>
+                  <DeleteOutlined />
+            </IconButton>
+        </ListItemSecondaryAction>
        </ListItem>
     )
   }
